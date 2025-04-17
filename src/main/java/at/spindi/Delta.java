d1cffbbb3baa85b8a6dc8ea494ccdf4b6b910848
Bernhard Spindler <bernhard.spindler@nts.eu>
2025-04-17 09:47:02 +0200

    restructured delta code

--- src/main/java/at/spindi/Delta.java
+++ src/main/java/at/spindi/Delta.java
@@ -1,6 +1,5 @@
 package at.spindi;
 
-import java.util.Comparator;
 import java.util.Iterator;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
@@ -30,53 +29,54 @@ public class Delta {
         A a = getNext(a_iter);
         B b = getNext(b_iter);
 
-        for (;;) {
-            
-            if ( a == null && b == null ) {
-                break;
+        while (a != null || b != null) {
+
+            switch (compare_keys(a, b, key_cmp_a_b)) {
+                case LESS:
+                    only_in_a.accept(a);
+                    a_last = a;
+                    a = getNext(a_iter);
+                    break;
+                case GREATER:
+                    only_in_b.accept(b);
+                    b_last = b;
+                    b = getNext(b_iter);
+                    break;
+                case EQUAL:
+                    if (attribute_cmp.test(a, b)) {
+                        samesame.accept(a, b);
+                    } else {
+                        modified.accept(a, b);
+                    }
+                    a_last = a;
+                    b_last = b;
+                    a = getNext(a_iter);
+                    b = getNext(b_iter);
+                    break;
             }
-            else if ( a != null && b == null ) {
-                only_in_a.accept(a);
-                a_last = a;
-                a = getNext(a_iter);
-            }
-            else if ( a == null && b != null ) {
-                only_in_b.accept(b);
-                b_last = b;
-                b = getNext(b_iter);
-            }
-            else {
-                switch (key_cmp_a_b.apply(a,b)) {
-                    case LESS :
-                        only_in_a.accept(a);
-                        a_last = a;
-                        a = getNext(a_iter);
-                        break;
-                    case GREATER :
-                        only_in_b.accept(b);
-                        b_last = b;
-                        b = getNext(b_iter);
-                        break;
-                    case EQUAL :
-                        if ( attribute_cmp.test(a,b) ) {
-                            samesame.accept(a,b);
-                        }
-                        else {
-                            modified.accept(a,b);
-                        }
-                        a_last = a;
-                        b_last = b;
-                        a = getNext(a_iter);
-                        b = getNext(b_iter);
-                }
-            }
-            if ( a != null ) {
+            if (a != null) {
                 check_sort_order(a_last, a, key_cmp_a_a, "A");
             }
-            if ( b != null ) {
+            if (b != null) {
                 check_sort_order(b_last, b, key_cmp_b_b, "B");
             }
         }
+    }
+
+    private static <A,B> Ordering compare_keys(A a, B b, BiFunction<A,B,Ordering> key_cmp_a_b) {
+        Ordering ordering;
+
+        if ( a != null && b == null ) {
+            ordering = Ordering.LESS;
+        }
+        else if ( a == null && b != null ) {
+            ordering = Ordering.GREATER;
+        }
+        else {
+            ordering = key_cmp_a_b.apply(a,b);
+        }
+
+        return ordering;
     }
 
     private static <A> A getNext(Iterator<A> iter) {
